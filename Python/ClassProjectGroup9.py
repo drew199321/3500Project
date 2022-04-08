d14e24e1a6ee8ff9db2d4fcee751e2e9209a5dc4
Zak <46013015+Zymbol@users.noreply.github.com>
2022-04-08 00:51:06 -0700

    Almost Finished Cleaning Data
    >Eliminate all rows with empty values in 3 or more columns
    >Eliminate all rows with distance equal to zero
    >Only consider in your analysis the first 5 digits of the zip code

--- Python/ClassProjectGroup9.py
+++ Python/ClassProjectGroup9.py
@@ -1,8 +1,18 @@
+############################################################################
+# Course: CMPS 3500
+# CLASS Project Group 9
+# PYTHON IMPLEMENTATION: BASIC DATA ANALYSIS
+# Date: 04/7/2022
+# Student 1: Zachary Scholefield
+# Student 2: Clemente Rodriguez
+# Student 4: Andrew Manz
+# Student 4: Scott Kurtz
+# Description: Implementation Basic Data Analysys Routines
+############################################################################
+
 import csv
 import pandas as pd
 from datetime import datetime
-
-
 
 print("Loading and cleaning input data set:")
 print("************************************")
@@ -13,17 +23,34 @@ print('[', datetime.now(), '] Loading US_Accidents_data.csv')
 # Read CSV file
 # Dataset = pd.read_csv("./Datasets/testing.csv")
 Dataset = pd.read_csv("./Datasets/US_Accidents_data.csv")
-print("Hello")
 df = pd.DataFrame(Dataset)
 # df.isnull()
 # Dataset = pd.read_csv("./Datasets/Loading US_Accidents_data.csv")
 
-df = df.dropna(subset=['ID', 'Severity', 'Zipcode', 'Start_Time',
-'End_Time', 'Visibility(mi)', 'Weather_Condition', 'Country'])
+############################################################################
+# CLEAN DATA
+############################################################################
 
-print(df)
-# print(df.dropna(subset=['Weather_Condition']))
+# Drop rows with missing data from any of the specified columns
+dropRowsSingle = df.dropna(subset=['ID', 'Severity', 'Zipcode', 'Start_Time',
+'End_Time', 'Visibility(mi)', 'Weather_Condition', 'Country'], inplace=True) 
 
-# print(datetime.now())
+# Drop Rows missing 3 columns
+dropRowsTriple = df.dropna(thresh=len(df.columns)-3, inplace=True)
+
+# Drop Rows where Distance = 0
+dropRowsDistance = df.drop(df.loc[df['Distance(mi)'] == 0].index)
+
+# df['Start_Time'] = pd.to_datetime(df['Start_Time'])
+# df['End_Time'] = pd.to_datetime(df['End_Time'])
+
+
+print(dropRowsSingle, dropRowsTriple, dropRowsDistance)
+
+# Only consider the first 5 digits of zipcode
+zipCoder = df['Zipcode'].str[:5]
+# Tested with index 21 with zipcode: 41033-9698; output: 41033
+print(zipCoder[21])
+
                 
 print ("DONE!")
