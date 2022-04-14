97e50d86ce9b0b86589d6c04be2f7fe8ba60d238
drew199321 <43482659+drew199321@users.noreply.github.com>
2022-04-13 19:34:08 -0700

    cleanup work

--- Ruby/Group9Ruby.rb
+++ Ruby/Group9Ruby.rb
@@ -2,11 +2,27 @@
 #! Partially require just CSV Importer
 require 'rubygems'
 require 'daru'
-# I do not  iknow what I can require for ruby to understand daru. 
-# It currently has a name error stating daru is an uninitialized variable 
+
+# Clean Values
 df = Daru::DataFrame.from_csv("US_Accidents_data.csv")
-df = df.reject_values nil, Float::NAN
-print "Ping\n"
+print "Ping1\n"
+print df.size
+print "\n"
+# df2 = df.reject_values nil
+df.filter_rows do |row|
+   !df['ID'].eq(nil) |
+   !df['Severity'].eq(nil) |
+   !df['Zipcode'].eq(nil) |
+   !df['Start_Time'].eq(nil) 
+   !df['End_Time'].eq(nil) |
+   !df['Visibility(mi)'].eq(nil) |
+   !df['Weather_Condition'].eq(nil) |
+   !df['Country'].eq(nil)
+end
+
+print "Ping2\n"
+print df.size
+print "\n"
 
 
 
