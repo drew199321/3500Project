1ea56116f5174fb151999f42279700e37478f183
drew199321 <43482659+drew199321@users.noreply.github.com>
2022-04-13 21:15:41 -0700

    Cleaned up process and removed dev stuff. first section done.

--- Ruby/Group9Ruby.rb
+++ Ruby/Group9Ruby.rb
@@ -4,51 +4,17 @@ require 'rubygems'
 require 'daru'
 
 # Clean Values
-df = Daru::DataFrame.from_csv("US_Accidents_data_quick.csv")
-print "Ping1\n"
+df = Daru::DataFrame.from_csv("US_Accidents_data.csv")
 print df.size
 print "\n"
-# df2 = df.reject_values nil
-   dropRowsID = df.where(!df['ID'].eq(nil) )
+   # Immeditately drop rows where certain attributes aren't present
    df = df.where(!df['ID'].eq(nil))
-   print dropRowsID
-   print "\n"
-
-   dropRowsSeverity = df.where(!df['Severity'].eq(nil))
    df = df.where(!df['Severity'].eq(nil))
-   print dropRowsSeverity
-   print "\n"
-
-   dropRowsZipcode = df.where(!df['Zipcode'].eq(nil))
    df = df.where(!df['Zipcode'].eq(nil))
-   print dropRowsZipcode
-   print "\n"
-
-   dropRowsStartTime = df.where(!df['Start_Time'].eq(nil))
    df = df.where(!df['Start_Time'].eq(nil))
-   print dropRowsStartTime
-   print "\n"
-
-   dropRowsEndTime = df.where(!df['End_Time'].eq(nil) )
    df = df.where(!df['End_Time'].eq(nil))
-   print dropRowsEndTime
-   print "\n"
-
-   dropRowsVisibility = df.where(!df['Visibility(mi)'].eq(nil))
    df = df.where(!df['Visibility(mi)'].eq(nil))
-   print dropRowsVisibility
-   print "\n"
-   
-   dropRowsWeatherCondition = df.where(!df['Weather_Condition'].eq(nil) )
-   df = df.where(!df['Weather_Condition'].eq(nil))
-   print dropRowsVisibility
-   print "\n"
-
    df = df.where(!df['Country'].eq(nil))  
-   dropRowsCountry = df.where(!df['Country'].eq(nil))
-   print dropRowsCountry
-   print "\n"
-print "Ping2\n"
 print df.size
 print "\n"
 
