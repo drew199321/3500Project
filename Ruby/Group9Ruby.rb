f0a8574d60795607ac7460c1a3efeb71eecdd7dc
drew199321 <43482659+drew199321@users.noreply.github.com>
2022-04-13 17:40:14 -0700

    Data Scrub work

--- Ruby/Group9Ruby.rb
+++ Ruby/Group9Ruby.rb
@@ -4,5 +4,12 @@ require 'rubygems'
 require 'daru'
 # I do not  iknow what I can require for ruby to understand daru. 
 # It currently has a name error stating daru is an uninitialized variable 
-df = DARU::DataFrame.new(
-Daru::DataFrame.from_csv("US_Accidents_data.csv")
+df = Daru::DataFrame.from_csv("US_Accidents_data.csv")
+df = df.reject_values nil, Float::NAN
+print "Ping\n"
+
+
+
+
+
+
