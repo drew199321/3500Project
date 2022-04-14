15208a05c11d87ee5b4795e40b58bffeccffd9b5
Scott Kurtz <scottkurtz8373@gmail.com>
2022-04-13 17:00:27 -0700

    understands daru and reads file

--- Ruby/Group9Ruby.rb
+++ Ruby/Group9Ruby.rb
@@ -2,7 +2,5 @@
 #! Partially require just CSV Importer
 require 'rubygems'
 require 'daru'
-# I do not  iknow what I can require for ruby to understand daru. 
-# It currently has a name error stating daru is an uninitialized variable 
-df = DARU::DataFrame.new(
-Daru::DataFrame.from_csv("US_Accidents_data.csv")
+df = Daru::DataFrame.new(
+Daru::DataFrame.from_csv("US_Accidents_data.csv"))
