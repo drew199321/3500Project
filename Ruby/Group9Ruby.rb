b149a64ca97314a9dff29f877498997860285c29
Scott Kurtz <scottkurtz8373@gmail.com>
2022-04-19 17:19:54 -0700

    some print and time stuff

--- Ruby/Group9Ruby.rb
+++ Ruby/Group9Ruby.rb
@@ -3,14 +3,14 @@
 require 'rubygems'
 require 'daru'
 
-# Clean Values
+print ("Loading and cleaning data set: \n")
+print ("************************************\n") 
 starting = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+print("The current time is: ", (starting))
+print ("Starting script\n")
+print("Performing clean up\n")
 df = Daru::DataFrame.from_csv("US_Accidents_data_quick.csv")
-print df.size
 print "\n"
-#df['Time_Elapsed'] = (df['Start_Time']- df['End_Time']) 
-#df['Time elapsed'] = df['Start_Time'] - df['End_Time']
-
 # Immediattely drop rows where certain attributes aren't present
 df = df.where(!df['ID'].eq(nil))
 df = df.where(!df['Severity'].eq(nil))
@@ -20,11 +20,26 @@ df = df.where(!df['End_Time'].eq(nil))
 df = df.where(!df['Visibility(mi)'].eq(nil))
 df = df.where(!df['Country'].eq(nil))  
 df = df.where(!df['Distance(mi)'].eq(0))  
+# eliminate rows where start and end time are the same. 
+# This appears to be an empty set
 df = df.where(!df['Start_Time'].eq(['End_Time']))  
 # need to eliminate rows with 3 or more nil/0 entries
-   print df.size
-print "\n"
+
+
 ending = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+print("The time after loading and cleaning is: ",(ending))
+print("The number of rows remaining after our cleaning process is: ") 
+print df.size
+print "\n"
 # #calculating elapsed time
 time = ending - starting
-print(time)
+print("Our process took ",(time)," seconds")
+print("\n")
+# now start answering questions
+
+currentTime = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+print"****************************************"
+print (currentTime), ("  The month with the most accidents :")
+
+
+ending = Process.clock_gettime(Process::CLOCK_MONOTONIC)
