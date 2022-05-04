31cbbb2e4f95a84965a00e6463b59d48bc354ffb
drew199321 <43482659+drew199321@users.noreply.github.com>
2022-05-03 17:35:44 -0700

    Menu Implemented
    Needs cleanup but starting to look good.

--- Python/ClassProjectGroup9.py
+++ Python/ClassProjectGroup9.py
@@ -10,7 +10,7 @@
 # Description: Implementation Basic Data Analysys Routines
 ############################################################################
 
-from asyncio.windows_events import NULL
+# from asyncio.windows_events import NULL
 import csv
 from numpy import average
 import pandas as pd
@@ -65,117 +65,180 @@ print ("CLEANING IS COMPLETE")
 ############################################################################
 
 # 1. In what month were there more accidents reported?
-print("Prompt 1")
-months = df['month'] = pd.DatetimeIndex(df['Start_Time']).month
-print(months.value_counts().head(1))
+def prompt1():
+    print("Prompt 1")
+    months = df['month'] = pd.DatetimeIndex(df['Start_Time']).month
+    print(months.value_counts().head(1))
 
 # 2. What is the state that had the most accidents in 2020?
-print("Prompt 2")
-years = df['years'] = pd.DatetimeIndex(df['Start_Time']).year
-acc2020 = df[years == 2020]
-
-state2020 = acc2020['State'].value_counts().head(1)
-print(state2020)
+def prompt2():
+    print("Prompt 2")
+    years = df['years'] = pd.DatetimeIndex(df['Start_Time']).year
+    acc2020 = df[years == 2020]
+    state2020 = acc2020['State'].value_counts().head(1)
+    print(state2020)
 
 # 3. What is the state that had the most accidents of severity 2 in 2021?
-print("Prompt 3")
-acc2021 = df[years == 2021]
-severity2 = acc2021['Severity'] == 2
-print(type(severity2))
-print(type(acc2021))
+def prompt3():
+    print("Prompt 3")
+    acc2021 = df[years == 2021]
+    severity2 = acc2021['Severity'] == 2
+    print(type(severity2))
+    print(type(acc2021))
 
-state1 = acc2021[severity2]
-state2 = state1['State']
-# state2 = severity2[acc2021]
-# print(acc2021)
-# print(severity2)
-# print(state1)
-print(state2)
-# print(df)
+    state1 = acc2021[severity2]
+    state2 = state1['State']
+    # state2 = severity2[acc2021]
+    # print(acc2021)
+    # print(severity2)
+    # print(state1)
+    print(state2)
+    # print(df)
 
 
 # 4. What severity is the most common in Virginia?
-print("Prompt 4")
-stateVirginia = df['State'] == "VA"
-virginiaColumns = df[stateVirginia]
-virginiaSeverity = virginiaColumns['Severity'].value_counts().head()
-# print(virginiaColumns)
-print(virginiaSeverity)
-# print(df)
+def prompt4():
+    print("Prompt 4")
+    stateVirginia = df['State'] == "VA"
+    virginiaColumns = df[stateVirginia]
+    virginiaSeverity = virginiaColumns['Severity'].value_counts().head()
+    # print(virginiaColumns)
+    print(virginiaSeverity)
+    # print(df)
 
 # 5. What are the 5 cities that had the most accidents in 2019 in California?
-print("Prompt 5")
-stateCalifornia = df['State'] == "CA"
-acc2019 = df[years == 2019]
-# print(acc2019)
-caliAccidents2019 = acc2019[stateCalifornia]
-topCaliforniaCities = caliAccidents2019['City'].value_counts().head() 
-print(topCaliforniaCities)
+def prompt5():
+    print("Prompt 5")
+    stateCalifornia = df['State'] == "CA"
+    acc2019 = df[years == 2019]
+    # print(acc2019)
+    caliAccidents2019 = acc2019[stateCalifornia]
+    topCaliforniaCities = caliAccidents2019['City'].value_counts().head() 
+    print(topCaliforniaCities)
 
 # 6. What was the average humidity and average temperature of all accidents of 
 # severity 4 that occurred in 2021?
-print("Prompt 6")
+def prompt6():
+    print("Prompt 6")
 
-# Severity of 4
-severityFour = df['Severity'] == 4
-# Severity of 4 in 2021
-yearAndSeverity = acc2021[severityFour]
-# Humidity of severity 4 accidents in 2021
-humiditySevFour = yearAndSeverity['Humidity(%)'].mean()
-# Temperature of severity 4 accidents in 2021
-tempSevFour = yearAndSeverity['Temperature(F)'].mean()
+    # Severity of 4
+    severityFour = df['Severity'] == 4
+    # Severity of 4 in 2021
+    yearAndSeverity = acc2021[severityFour]
+    # Humidity of severity 4 accidents in 2021
+    humiditySevFour = yearAndSeverity['Humidity(%)'].mean()
+    # Temperature of severity 4 accidents in 2021
+    tempSevFour = yearAndSeverity['Temperature(F)'].mean()
 
-# average temperature and humidity for accidents w/ a severity of 4 in 2021
-print('average humidity: ', humiditySevFour)
-print('average temp: ', tempSevFour)
+    # average temperature and humidity for accidents w/ a severity of 4 in 2021
+    print('average humidity: ', humiditySevFour)
+    print('average temp: ', tempSevFour)
 
 # 7. What are the 3 most common weather conditions (weather_conditions) when accidents occurred?
-print("Prompt 7")
-weatherConditions = df['Weather_Condition'].value_counts().head(3) 
-print(weatherConditions)
+def prompt7():
+    print("Prompt 7")
+    weatherConditions = df['Weather_Condition'].value_counts().head(3) 
+    print(weatherConditions)
 
 # 8. What was the maximum visibility of all accidents of severity 2 that occurred in the state of New Hampshire?
-print("Prompt 8")
-stateNewHampshire = df['State'] == "NH"
-tempDF = df[stateNewHampshire]
-severity = df['Severity'] == 2
-stateSeverity = tempDF[severity]
-visibility = stateSeverity['Visibility(mi)'].max()
-# print(df)
-print(visibility)
+def prompt8():
+    print("Prompt 8")
+    stateNewHampshire = df['State'] == "NH"
+    tempDF = df[stateNewHampshire]
+    severity = df['Severity'] == 2
+    stateSeverity = tempDF[severity]
+    visibility = stateSeverity['Visibility(mi)'].max()
+    # print(df)
+    print("The maximum visibility of all accidents of severity 2 that occurred in the state of New Hampshire:")
+    print(visibility)
 
 # 9. How many accidents of each severity were recorded in Bakersfield?
-print("Prompt 9")
-bakersfield = df['City'] == "Bakersfield"
-severityBak1 = df['Severity'] == 1
-severityBak2 = df['Severity'] == 2
-severityBak3 = df['Severity'] == 3
-severityBak4 = df['Severity'] == 4
+def prompt9():
+    print("Prompt 9")
+    bakersfield = df['City'] == "Bakersfield"
+    severityBak1 = df['Severity'] == 1
+    severityBak2 = df['Severity'] == 2
+    severityBak3 = df['Severity'] == 3
+    severityBak4 = df['Severity'] == 4
 
-tmpDF = df[bakersfield]
+    tmpDF = df[bakersfield]
 
-print(df)
-print(severityBak1)
-print(type(severityBak1))
-print(type(tmpDF))
+    print(df)
+    print(severityBak1)
+    print(type(severityBak1))
+    print(type(tmpDF))
 
-bakersfieldSev1 = len(tmpDF[severityBak1])
-bakersfieldSev2 = len(tmpDF[severityBak2])
-bakersfieldSev3 = len(tmpDF[severityBak3])
-bakersfieldSev4 = len(tmpDF[severityBak4])
+    bakersfieldSev1 = len(tmpDF[severityBak1])
+    bakersfieldSev2 = len(tmpDF[severityBak2])
+    bakersfieldSev3 = len(tmpDF[severityBak3])
+    bakersfieldSev4 = len(tmpDF[severityBak4])
 
-print(type(bakersfieldSev1))
-print("Accidents in Bakersfield with Severity 1: ", bakersfieldSev1)
-print("Accidents in Bakersfield with Severity 2: ", bakersfieldSev2)
-print("Accidents in Bakersfield with Severity 3: ", bakersfieldSev3)
-print("Accidents in Bakersfield with Severity 4: ", bakersfieldSev4)
+    print(type(bakersfieldSev1))
+    print("Accidents in Bakersfield with Severity 1: ", bakersfieldSev1)
+    print("Accidents in Bakersfield with Severity 2: ", bakersfieldSev2)
+    print("Accidents in Bakersfield with Severity 3: ", bakersfieldSev3)
+    print("Accidents in Bakersfield with Severity 4: ", bakersfieldSev4)
 
 # 10 What was the longest accident (in hours) recorded in Florida in the Spring (March, April, and May) of 2022?
-print("Prompt 10")
+def prompt10():
+    print("Prompt 10")
 
-stateFlorida = df['State'] == 'FL'
-floridaColumns = df[stateFlorida]
-acc2022 = df[years == 2022]
+    stateFlorida = df['State'] == 'FL'
+    floridaColumns = df[stateFlorida]
+    acc2022 = df[years == 2022]
+
+def menu_selection(action):
+    if(action =='1'):
+            prompt1()
+            return False
+    if(action =='2'):
+            prompt2()
+            return False
+    if(action =='3'):
+            prompt3()
+            return False
+    if(action =='4'):
+            prompt4()
+            return False
+    if(action =='5'):
+            prompt5()
+            return False
+    if(action =='6'):
+            prompt6()
+            return False
+    if(action =='7'):
+            prompt7()
+            return False
+    if(action =='8'):
+            prompt8()
+            return False
+    if(action =='9'):
+            prompt9()
+            return False
+    if(action =='10'):
+            prompt10()
+            return False
+    if(action =='q'):
+            return True
+    else:
+            print("Error: Invalid input. Please try again.")
+            return False
+exit = False
+while(exit == False):
+    print("Please enter the number of a prompt from the following menu:")
+    print("1: In what month were there more accidents reported?")
+    print("2: What is the state that had the most accidents in 2020?")
+    print("3: What is the state that had the most accidents of severity 2 in 2021?")
+    print("4. What severity is the most common in Virginia?")
+    print("5. What are the 5 cities that had the most accidents in 2019 in California?")
+    print("6. What was the average humidity and average temperature of all accidents of severity 4 that occurred in 2021?")
+    print("7. What are the 3 most common weather conditions (weather_conditions) when accidents occurred?")
+    print("8. What was the maximum visibility of all accidents of severity 2 that occurred in the state of New Hampshire?")
+    print("9. How many accidents of each severity were recorded in Bakersfield?")
+    print("10. What was the longest accident (in hours) recorded in Florida in the Spring (March, April, and May) of 2022? **CURRENTLY BROKEN**")
+    action = input()
+    exit = menu_selection(action)
+
 
 
 
