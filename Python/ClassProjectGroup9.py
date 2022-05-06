697bc7303be1b38577190c5c64fa8513413070bd
Scott Kurtz <scottkurtz8373@gmail.com>
2022-05-05 21:37:34 -0700

    chnged list because Walter changed the project

--- Python/ClassProjectGroup9.py
+++ Python/ClassProjectGroup9.py
@@ -5,7 +5,7 @@
 # Date: 04/7/2022
 # Student 1: Zachary Scholefield
 # Student 2: Clemente Rodriguez Jr
-# Student 4: Andrew Manz
+# Student 3: Andrew Manz
 # Student 4: Scott Kurtz
 # Description: Implementation Basic Data Analysys Routines
 ############################################################################
@@ -16,54 +16,37 @@ from numpy import average
 import pandas as pd
 from datetime import datetime
 
-print("Loading and cleaning input data set:")
-print("************************************")
-# Dataset = pd.read_csv("./Datasets/InputDataSample.csv")
-print('[', datetime.now(), '] Starting Script')
-# print('[', datetime.now(), '] Loading US_Accidents_data.csv')
+def loadData():
+    print("Loading and cleaning input data set:")
+    print("************************************")
+    # Dataset = pd.read_csv("./Datasets/InputDataSample.csv")
+    print('[', datetime.now(), '] Starting Script')
+    Dataset = pd.read_csv("./Datasets/US_Accidents_data.csv")
+    df = pd.DataFrame(Dataset)
 
-# Read CSV file
-# Dataset = pd.read_csv("./Datasets/testing.csv")
-Dataset = pd.read_csv("./Datasets/US_Accidents_data.csv")
-df = pd.DataFrame(Dataset)
-# df.isnull()
-# Dataset = pd.read_csv("./Datasets/Loading US_Accidents_data.csv")
-
-############################################################################
 # CLEAN DATA
-############################################################################
+def processData():
+    # Drop rows with missing data from any of the specified columns
+    dropRowsSingle = df.dropna(subset=['ID', 'Severity', 'Zipcode', 'Start_Time',
+    'End_Time', 'Visibility(mi)', 'Weather_Condition', 'Country'], inplace=True) 
 
-# Drop rows with missing data from any of the specified columns
-dropRowsSingle = df.dropna(subset=['ID', 'Severity', 'Zipcode', 'Start_Time',
-'End_Time', 'Visibility(mi)', 'Weather_Condition', 'Country'], inplace=True) 
+    # Drop Rows missing 3 columns
+    dropRowsTriple = df.dropna(thresh=len(df.columns)-2, inplace=True)
 
-# Drop Rows missing 3 columns
-dropRowsTriple = df.dropna(thresh=len(df.columns)-2, inplace=True)
+    # Drop Rows where Distance = 0
+    df = df.drop(df.loc[df['Distance(mi)'] == 0].index)
 
-# Drop Rows where Distance = 0
-df = df.drop(df.loc[df['Distance(mi)'] == 0].index)
-
-# Drop rows where start and endtime are the same(equal zero)
-df['Start_Time'] = pd.to_datetime(df['Start_Time'])
-df['End_Time'] = pd.to_datetime(df['End_Time'])
-df = df[df['Start_Time'] != df['End_Time']]
-
-
-
+    # Drop rows where start and endtime are the same(equal zero)
+    df['Start_Time'] = pd.to_datetime(df['Start_Time'])
+    df['End_Time'] = pd.to_datetime(df['End_Time'])
+    df = df[df['Start_Time'] != df['End_Time']]
 
 # # Only consider the first 5 digits of zipcode
-
 # zipCoder = df['Zipcode'].str[:5]
 # # Tested with index 21 with zipcode: 41033-9698; output: 41033
 # print(zipCoder[21])
-
-                
-print ("CLEANING IS COMPLETE")
-
-############################################################################
-# OUTPUT PROMPTS
-############################################################################
-
+    print ("CLEANING IS COMPLETE")
+# starting functions that answer the questions
 # 1. In what month were there more accidents reported?
 def prompt1():
     print("Prompt 1")
@@ -102,15 +85,13 @@ def prompt4():
     stateVirginia = df['State'] == "VA"
     virginiaColumns = df[stateVirginia]
     virginiaSeverity = virginiaColumns['Severity'].value_counts().head()
-    # print(virginiaColumns)
     print(virginiaSeverity)
-    # print(df)
 
 # 5. What are the 5 cities that had the most accidents in 2019 in California?
 def prompt5():
     print("Prompt 5")
     stateCalifornia = df['State'] == "CA"
-    acc2019 = df[years == 2019]
+    acc2019 = df[ears == 2019]
     # print(acc2019)
     caliAccidents2019 = acc2019[stateCalifornia]
     topCaliforniaCities = caliAccidents2019['City'].value_counts().head() 
@@ -187,38 +168,39 @@ def prompt10():
     floridaColumns = df[stateFlorida]
     acc2022 = df[years == 2022]
 
+# end of functions for the promps/questions, resume functions for list 3 to 7 
+def printAnswers():
+    print("these are the answers")
+
+def searchAccidentsPlace():
+    print("we will prompt user to choose place")
+
+    def searchAccidentTime():
+        print("search by time")
+
+        def searchAccidentConditions():
+            print("Search by conditions")
+
 def menu_selection(action):
     if(action =='1'):
-            prompt1()
+            loadData()
             return False
     if(action =='2'):
-            prompt2()
+            processData()
             return False
     if(action =='3'):
-            prompt3()
+            printAnswers()
             return False
     if(action =='4'):
-            prompt4()
+            searchAccidentsPlace()
             return False
     if(action =='5'):
-            prompt5()
+            searchAccidentsTime()
             return False
     if(action =='6'):
-            prompt6()
+            searchAccidentsConditions()
             return False
     if(action =='7'):
-            prompt7()
-            return False
-    if(action =='8'):
-            prompt8()
-            return False
-    if(action =='9'):
-            prompt9()
-            return False
-    if(action =='10'):
-            prompt10()
-            return False
-    if(action =='q'):
             return True
     else:
             print("Error: Invalid input. Please try again.")
@@ -226,16 +208,13 @@ def menu_selection(action):
 exit = False
 while(exit == False):
     print("Please enter the number of a prompt from the following menu:")
-    print("1: In what month were there more accidents reported?")
-    print("2: What is the state that had the most accidents in 2020?")
-    print("3: What is the state that had the most accidents of severity 2 in 2021?")
-    print("4. What severity is the most common in Virginia?")
-    print("5. What are the 5 cities that had the most accidents in 2019 in California?")
-    print("6. What was the average humidity and average temperature of all accidents of severity 4 that occurred in 2021?")
-    print("7. What are the 3 most common weather conditions (weather_conditions) when accidents occurred?")
-    print("8. What was the maximum visibility of all accidents of severity 2 that occurred in the state of New Hampshire?")
-    print("9. How many accidents of each severity were recorded in Bakersfield?")
-    print("10. What was the longest accident (in hours) recorded in Florida in the Spring (March, April, and May) of 2022? **CURRENTLY BROKEN**")
+    print("1: Load  the data")
+    print("2: Process the data")
+    print("3: Print the answers to the questions")
+    print("4: Search accidents by place (city, state, zip)") 
+    print("5: Search accidents by time(year, month, day)")
+    print("6: Search accidents by conditions (temperature range and visibility range)")
+    print("7: Quit")
     action = input()
     exit = menu_selection(action)
 
